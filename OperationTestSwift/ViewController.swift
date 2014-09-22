26d9bf61a87e687fc8a1d1c9ccec696e52c249de
robertmryan <robert.ryan@mindspring.com>
2014-09-22 11:25:05 -0400

    Initial Commit

---
+++ OperationTestSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OperationTestSwift
+//
+//  Created by Robert Ryan on 9/22/14.
+//  Copyright (c) 2014 Robert Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
